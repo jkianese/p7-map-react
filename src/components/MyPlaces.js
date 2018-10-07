c1fae13f5752663cecc6c22d8a7225755edfa826
[jkianese] <[jkianese@gmail.com]>
2018-10-07 19:56:24 -0400

    add new components

---
+++ src/components/MyPlaces.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import SideBar from './SideBar';
+
+class MyPlaces extends Component {
+
+    render() {
+        return(
+            <div>
+                
+            </div>       
+        )
+    }
+
+    /*
+    render() {
+        return (
+            <li role="button" className="box" tabIndex="0" 
+            onKeyPress={this.props.openInfoWindow.bind(this, this.props.data.marker)} 
+            onClick={this.props.openInfoWindow.bind(this, this.props.data.marker)}></li>
+        );
+        
+    }
+    */
+
+}
+
+export default MyPlaces;
