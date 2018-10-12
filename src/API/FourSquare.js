00eff314999174a12daa23141e58a709679fadf9
Jim Kianese <jkianese@gmail.com>
2018-10-12 05:16:55 -0400

    clean up code
    clean up code

--- src/API/FourSquare.js
+++ src/API/FourSquare.js
@@ -4,8 +4,8 @@ class fsHelper {
     }
     static auth() {
         const keys = {
-            client_id: "F1BIU3KU3RZFBQKCLKJ2MX1AT2ZRZFYRUXTJUMFGA1YUS5ZF",
-            client_secret: "N1HLP0MSWYJJATAS3CBQTTSZ2WLME5RB2TAUWHGE2UXZ5A1E",
+            client_id: "",
+            client_secret: "",
             query: "haunted places",
             ll: "40.4127981, -80.0013632",
             limit: 13,
